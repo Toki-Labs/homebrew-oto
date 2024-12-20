bfa67c57101b3b02bf5013005db5d32347f9cd32
Toki <dongmyung.5152@gmail.com>
2024-12-21 08:03:57 +0900

    modify code

--- Formula/oto.rb
+++ Formula/oto.rb
@@ -16,7 +16,7 @@ class Oto < Formula
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     # system "./configure", "--disable-silent-rules", *std_configure_args
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    bin.install "bin/oto_cli"
+    bin.install "oto_cli"
   end
 
   test do
@@ -29,6 +29,6 @@ class Oto < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system bin/"program", "do", "something"`.
-    system "{bin}/oto_cli", "template", "-h"
+    system "#{bin}/oto_cli", "template", "-h"
   end
 end
