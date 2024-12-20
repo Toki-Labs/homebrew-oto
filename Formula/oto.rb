9db9001cc35851120332a2a84f29e5a1419b6035
Toki <dongmyung.5152@gmail.com>
2024-12-21 07:19:03 +0900

    init push

---
+++ Formula/oto.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Oto < Formula
+  desc ""
+  homepage ""
+  url "https://toki-labs.com/cdn/oto/oto_cli.tar.gz"
+  version "0.0.1"
+  sha256 "b2b0cc844205036927ec565af5b68cf6025ab64c2d5c32ef446176c5229f42f6"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "bin/oto_cli"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test oto_cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "{bin}/oto_cli", "template", "-h"
+  end
+end
