94791c350f503fa8cd7a49455c2178dcc5d72d9e
toki <dongmyung.5152@gmail.com>
2025-08-22 02:50:49 +0000

    Update file version 0.0.331

--- Formula/oto.rb
+++ Formula/oto.rb
@@ -20,8 +20,8 @@ class Oto < Formula
       url "https://toki-labs.com/cdn/oto/linux/oto_0.0.232.tar.gz"
       sha256 "89c24f593ef1e36dcb48bfc5afc33a2aadce119274443da14515008495b9fbb4"
     elsif Hardware::CPU.intel? # Intel/AMD 아키텍처인 경우
-      url "https://toki-labs.com/cdn/oto/linux/oto_x64_0.0.330.tar.gz"
-      sha256 "55d8833473bf13e80d197bad89389e11e4ecf35495c07f6722db7033a33ce51a"
+      url "https://toki-labs.com/cdn/oto/linux/oto_x64_0.0.331.tar.gz"
+      sha256 "11395fd9ebd665fc4c5232d97397e44e22ff9ed7467ee096af1916616f28947a"
     end
   end
 
