a159754ba3d90feb51dee3adbffa95a848587f83
leedongmyung <dongmyung.5152@gmail.com>
2026-04-19 19:04:27 +0900

    Update file version 0.0.371

--- Formula/oto.rb
+++ Formula/oto.rb
@@ -4,13 +4,13 @@
 class Oto < Formula
   desc "oto Automation tool"
   homepage "https://toki-labs.com/oto"
-  version "0.0.370"
+  version "0.0.371"
   license ""
 
   if OS.mac?
     if Hardware::CPU.arm? # Apple Silicon (arm64) 아키텍처인 경우
-      url "https://toki-labs.com/cdn/oto/osx/oto_arm_0.0.370.tar.gz"
-      sha256 "a603860b5bda0a9375bde989a5698416eb1158e08290331f194459af6bc346bf"
+      url "https://toki-labs.com/cdn/oto/osx/oto_arm_0.0.371.tar.gz"
+      sha256 "d672b949974eb1ac27bfe9137b633c9b15a5b463a840081347d5f48ff059721c"
     elsif Hardware::CPU.intel? # Intel (x86_64) 아키텍처인 경우
       url "https://toki-labs.com/cdn/oto/osx/oto_0.0.232.tar.gz"
       sha256 "89c24f593ef1e36dcb48bfc5afc33a2aadce119274443da14515008495b9fbb4"
